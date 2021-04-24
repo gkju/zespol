a42a8213c476ca5d9c98efe573628c0663431b11
gkju <gkjuszczyk@gmail.com>
2021-04-24 23:32:08 +0200

    Initial commit

---
+++ zespol-gui/App.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Windows.ApplicationModel.Appointments;
+
+namespace ZespolWpfGui
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Properties["test"] = 12;
+        }
+
+        private readonly string propertiesStoreFileName = "properties_store";
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            IsolatedStorageFile storage = IsolatedStorageFile.GetUserStoreForDomain();
+            try
+            {
+                using IsolatedStorageFileStream stream =
+                    new IsolatedStorageFileStream(propertiesStoreFileName, FileMode.Open, storage);
+                using StreamReader reader = new StreamReader(stream);
+                // restoring properties
+                string jsonString = reader.ReadToEnd();
+                Dictionary<string, object> properties =
+                    JsonSerializer.Deserialize<Dictionary<string, object>>(jsonString);
+                foreach (var propertyPair in properties)
+                {
+                    this.Properties[propertyPair.Key] = propertyPair.Value;
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                // properties not found
+            }
+            catch (JsonException ex)
+            {
+                   // 
+            }
+        }
+
+        private void App_Exit(object sender, ExitEventArgs e)
+        {
+            IsolatedStorageFile storage = IsolatedStorageFile.GetUserStoreForDomain();
+            using (IsolatedStorageFileStream stream = new IsolatedStorageFileStream(propertiesStoreFileName, FileMode.Create, storage))
+            using (StreamWriter writer = new StreamWriter(stream))
+            {
+                Dictionary<string, object> properties = new Dictionary<string, object>();
+                 
+                foreach (string key in this.Properties.Keys)
+                {
+                    properties.Add(key, Properties[key]);
+                }
+
+                string jsonString = JsonSerializer.Serialize(properties);
+                writer.Write(jsonString);
+            }
+        }
+    }
+}
---
+++ zespol-gui/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ZespolWpfGui.Settings;
+using HandyControl.Themes;
+
+
+namespace ZespolWpfGui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<ZespolFile> ZespolFiles { get; set; } = new ObservableCollection<ZespolFile>();
+        public ObservableCollection<ServerRemote> ServerRemotes { get; set; } = new ObservableCollection<ServerRemote>();
+        public ZespolFile SelectedFile { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            try
+            {
+                ServerRemotes = new ObservableCollection<ServerRemote>((List<ServerRemote>) Application.Current.Properties["ServerRemotes"]);
+            }
+            catch
+            {
+
+            }
+            try
+            {
+                ZespolFiles = new ObservableCollection<ZespolFile>((List<ZespolFile>) Application.Current.Properties["ZespolFiles"]);
+            }
+            catch
+            {
+
+            }
+
+            ZespolFiles = new ObservableCollection<ZespolFile>()
+                {new ZespolFile {Description = "waweaweaw", Name = "fajny plik"}};
+
+            ZespolFilesList.ItemsSource = ZespolFiles;
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void OpenFile(ZespolFile selectedFile)
+        {
+            ZespolFiles.Add(new ZespolFile {Description = selectedFile.Name, Name = selectedFile.Description});
+        }
+
+        private void OpenFileDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            OpenFile(SelectedFile);
+        }
+
+        private void OpenFileButton(object sender, RoutedEventArgs e)
+        {
+            ZespolFile SelectedFile =
+                ZespolFiles.Where(file => file.FilePath == ((Button) sender).Tag).FirstOrDefault();
+            OpenFile(SelectedFile);
+        }
+    }
+}
